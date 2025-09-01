dd23b4695925f723795f8d8647f71a294bdd128b
daviixs <xaviersilvadavi@gmail.com>
2025-09-01 16:53:08 -0300

    Inicio

---
+++ src/main/java/com/seucabelo/seucabelo/SeucabeloApplication.java
@@ -0,0 +1,13 @@
+package com.seucabelo.seucabelo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeucabeloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeucabeloApplication.class, args);
+	}
+
+}
